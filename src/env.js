8bc5f94906694a3872f3827ad2121737203d7b0a
Riley Barabash <admin@rileybarabash.com>
2024-07-19 03:09:45 -0600

    chore: clean up `env.js`

--- src/env.js
+++ src/env.js
@@ -1,42 +1,52 @@
+/**
+ * @file Validates environment variables against a Zod schema.
+ * @author Riley Barabash <riley@rileybarabash.com>
+ *
+ * @tags
+ * - #environment
+ * - #validation
+ * - #zod
+ *
+ * @todo
+ * - [P3] Create a custom error class for ENV errors.
+ * - [P4] Consider building a custom implementation with Zod.
+ * - [P2] Revert `POSTGRES_URL` to `DATABASE_URL`.
+ *
+ * @see [T3 Environment Variables](https://create.t3.gg/en/usage/env-files)
+ */
+
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
     /**
-     * Specify your server-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars.
+     * These environment variables are available server-side only.
      */
     server: {
-        POSTGRES_URL: z.string().url(),
-        NODE_ENV: z.enum(["development", "test", "production"]).default("development")
+        NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+        POSTGRES_URL: z.string().url()
     },
 
     /**
-     * Specify your client-side environment variables schema here. This way you can ensure the app
-     * isn't built with invalid env vars. To expose them to the client, prefix them with
-     * `NEXT_PUBLIC_`.
+     * These environment variables are exposed to the client only. Make sure to prefix each with `NEXT_PUBLIC_`.
      */
-    client: {
-        // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    },
+    client: {},
 
     /**
-     * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-     * middlewares) or client-side so we need to destruct manually.
+     * You can't access `process.env` as a regular object in the Next.js edge runtimes (e.g., middlewares) or client-side so we need to expose them manually.
      */
     runtimeEnv: {
-        POSTGRES_URL: process.env.POSTGRES_URL,
-        NODE_ENV: process.env.NODE_ENV
-        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+        NODE_ENV: process.env.NODE_ENV,
+        POSTGRES_URL: process.env.POSTGRES_URL
     },
+
     /**
-     * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-     * useful for Docker builds.
+     * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip ENV validation. This is especially useful for Docker builds.
      */
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
     /**
-     * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-     * `SOME_VAR=''` will throw an error.
+     * Makes it so that empty strings are treated as undefined, throwing an error when validated with `z.string`.
      */
     emptyStringAsUndefined: true
 })
