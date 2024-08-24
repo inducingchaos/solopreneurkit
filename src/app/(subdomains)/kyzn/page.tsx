7737ddb5a1696d87d5ffdb2dffcf5d5e5aedd41a
Riley Barabash <admin@rileybarabash.com>
2024-08-23 20:27:05 -0600

    feat: Kyzn domain prep
    Added directories for Kyzn.

---
+++ src/app/(subdomains)/kyzn/page.tsx
@@ -0,0 +1,33 @@
+/**
+ * @file The marketing site for Kyzn.
+ * @author Riley Barabash <riley@rileybarabash.com>
+ *
+ * @tags
+ * - #component
+ * - #page
+ * - #marketing
+ * - #home
+ * - #landing
+ * - #tsx
+ */
+
+/**
+ * The home page for new users of Kyzn.
+ */
+export default function Home(): JSX.Element {
+    return (
+        <>
+            {/* Main tag. */}
+
+            <main className="container flex min-h-screen w-full flex-col items-center justify-center">
+                {/* Section one. */}
+
+                <section className="flex h-screen w-full flex-col items-center justify-center">
+                    {/* Placeholder content. */}
+
+                    <p className="">Kyzn</p>
+                </section>
+            </main>
+        </>
+    )
+}
